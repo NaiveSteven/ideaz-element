b9628a24ee23722c6356f9ebbea245fcfa5b32a8
NaiveSteven <1004537433@qq.com>
2023-04-21 00:18:29 +0800

    feat: add z-row component

--- packages/element/index.ts
+++ packages/element/index.ts
@@ -1,9 +1,10 @@
 import ZCheckbox from './checkbox';
 import ZRadio from './radio';
 import ZSelect from './select';
+import ZRow from './row';
 import { makeInstaller } from './makeInstaller';
 
-const components = [ZCheckbox, ZRadio, ZSelect];
+const components = [ZCheckbox, ZRadio, ZSelect, ZRow];
 
 // const install = function (Vue: App) {
 //   components.forEach((component) => {
@@ -16,4 +17,4 @@ const installer = makeInstaller([...components]);
 export default installer;
 export * from './types';
 
-export { ZRadio, ZSelect, ZCheckbox };
+export { ZRadio, ZSelect, ZCheckbox, ZRow };
