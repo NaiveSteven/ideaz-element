c41113e1e229f89206e8c077a31ae98585a39be8
NaiveSteven <1004537433@qq.com>
2023-04-21 00:31:58 +0800

    feat: add z-col component

---
+++ packages/element/col/index.ts
@@ -0,0 +1,4 @@
+import { withInstall } from '@ideaz/utils';
+import Col from './src/index.tsx';
+
+export default withInstall(Col);
--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -3,3 +3,4 @@ export * from './toCamelCase';
 export * from './toKebabCase';
 export * from './getEventsFromCamel';
 export * from './withinstall';
+export * from './runtime';
