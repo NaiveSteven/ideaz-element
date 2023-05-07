1b53bc07130e32859caeb709dccfd07a65d4a3c7
NaiveSteven <1004537433@qq.com>
2023-05-08 00:41:25 +0800

    feat: z-tag-select support all

--- packages/element/tag-select/src/index.tsx
+++ packages/element/tag-select/src/index.tsx
@@ -20,26 +20,36 @@ export default defineComponent({
       type: String,
       default: '',
     },
+    all: {
+      type: Boolean,
+      default: true,
+    },
   },
   emits: ['change', 'update:modelValue'],
   setup(props, { emit }) {
-    const activeTag = computed<number[] | number | string>({
+    const activeTag = computed<number[] | number | string | string[]>({
       get() {
         return props.modelValue
       },
-      set(val: number[] | number | string) {
+      set(val: number[] | number | string | string[]) {
         emit('update:modelValue', val)
       },
     })
 
+    const options = computed(() => {
+      if (props.all && props.multiple)
+        return [{ label: '全部', value: 'all' }, ...props.options]
+
+      return props.options
+    })
+
     const observer = elementResizeDetectorMaker()
+    const isShowMore = ref(false)
 
     const { width } = useWindowSize()
 
-    const zTag = ref(null)
-
     watch(() => width.value, () => {
-      computedMore()
+      // computedMore()
     })
 
     onMounted(() => {
@@ -48,69 +58,61 @@ export default defineComponent({
       //   document.querySelector('.z-tag-select__container'),
       //   computedMore,
       // )
-      computedMore()
+      // computedMore()
     })
 
-    function computedMore(ele?: any) {
-      const element = ele || document.getElementsByClassName('z-tag-select__container')[0]
-      const tagTitle = document.getElementsByClassName('z-tag-select__title')[0]
-      if (element) {
-        const width = element.offsetWidth
-        const tags = element.querySelectorAll('.el-tag')
-        const originTagWidth = tags[0].offsetWidth
-        let totalWidth = 0 + (tagTitle ? tagTitle.offsetWidth + 20 : 0)
-        let index = 0
-        for (let i = 0; i < tags.length; i++) {
-          // const tag = tags[i]
-          totalWidth = totalWidth + originTagWidth + 16
-          if (totalWidth > width) {
-            index = i
-            break
-          }
-        }
-        // console.log(totalWidth, width, index, tags, 'asdf')
-        if (index > 0) {
-          showHideMore(tags, 'hide', index)
-          let more = element.querySelector('.z-tag-select__more')
-          if (!more)
-            more = document.createElement('span')
-
-          more.innerHTML = `+${tags.length - index}`
-          more.style.marginLeft = '4px'
-          more.style.cursor = 'pointer'
-          more.style.position = 'absolute'
-          more.style.top = '1px'
-          more.style.right = '0'
-          more.className += ' z-tag-select__more'
-          more.onclick = () => {
-            if (more.innerHTML.startsWith('+')) {
-              showHideMore(tags, 'show', index)
-              more.innerHTML = `-${tags.length - index}`
-            }
-            else {
-              showHideMore(tags, 'hide', index)
-              more.innerHTML = `+${tags.length - index}`
-            }
-          }
-          element.appendChild(more)
-        }
+    const cls = computed(() => {
+      return {
+        'z-tag-select__container': true,
+        'z-tag-select__container--more': isShowMore.value,
       }
-    }
+    })
 
-    function showHideMore(tags: any, operate: 'show' | 'hide', index: number) {
-      for (let i = 0; i < tags.length; i++) {
-        const tag = tags[i]
-        if (operate === 'hide') {
-          if (i < index)
-            tag.style.display = 'inline-flex'
-
-          else
-            tag.style.display = 'none'
-        }
-        else {
-          tag.style.display = 'inline-flex'
-        }
+    const iconClass = computed(() => {
+      return {
+        'z-toggle__icon': true,
+        'z-icon__arrow': isShowMore.value,
       }
+    })
+
+    // function computedMore(ele?: any) {
+    //   const element = ele || document.getElementsByClassName('z-tag-select__container')[0]
+    //   const tagTitle = document.getElementsByClassName('z-tag-select__title')[0]
+    //   if (element) {
+    //     const width = element.offsetWidth
+    //     const tags = element.querySelectorAll('.el-tag')
+    //     // const originTagWidth = props.all ? tags[1].offsetWidth : tags[0].offsetWidth
+    //     let totalWidth = 0 + (tagTitle ? tagTitle.offsetWidth + 20 : 0)
+    //     let index = 0
+    //     for (let i = 0; i < tags.length; i++) {
+    //       const tag = tags[i]
+    //       totalWidth = totalWidth + tag.offsetWidth + 16
+    //       if (totalWidth > width) {
+    //         index = i
+    //         break
+    //       }
+    //     }
+    //     if (index > 0) {
+    //       showHideMore(tags, 'hide', index)
+    //       if (!more) {
+    //         more.onclick = () => {
+    //           if (more.innerHTML.startsWith('+')) {
+    //             showHideMore(tags, 'show', index)
+    //             isShowMore.value = true
+    //           }
+    //           else {
+    //             showHideMore(tags, 'hide', index)
+    //             isShowMore.value = false
+    //           }
+    //         }
+    //       }
+    //       element.appendChild(more)
+    //     }
+    //   }
+    // }
+
+    const handleChangeExpand = () => {
+      isShowMore.value = !isShowMore.value
     }
 
     const isActive = (item: any) => {
@@ -120,8 +122,33 @@ export default defineComponent({
       return String(activeTag.value) === String(item.value) ? 'dark' : 'light'
     }
 
-    const handleClick = (item: any) => {
+    const handleClickTag = (item: any) => {
       const effect = isActive(item)
+      if (item.value === 'all') {
+        if (effect === 'dark') {
+          if (props.multiple) {
+            activeTag.value = []
+            emit('change', [])
+            return
+          }
+          else {
+            activeTag.value = ''
+            emit('change', item)
+          }
+        }
+        else {
+          if (props.multiple) {
+            activeTag.value = options.value.map((item: any) => item.value)
+            emit('change', options.value.map((item: any) => item.value))
+            return
+          }
+          else {
+            activeTag.value = item.value
+            emit('change', item)
+          }
+        }
+        return
+      }
       if (props.multiple) {
         const actives = activeTag.value as number[]
         if (effect === 'dark') {
@@ -142,14 +169,35 @@ export default defineComponent({
           emit('change', item)
         }
       }
+      // compute is select all or not
+      if (props.multiple) {
+        const actives = activeTag.value as string[]
+        const values = options.value.map((item: any) => item.value)
+        if (actives.includes('all')) {
+          if (actives.length !== values.length) {
+            const index = actives.indexOf('all')
+            actives.splice(index, 1)
+            emit('change', values)
+          }
+        }
+        else {
+          if (actives.length === values.length - 1) {
+            activeTag.value = values
+            emit('change', values)
+          }
+        }
+      }
     }
 
     return () => {
-      return <div ref={zTag.value} class='z-tag-select__container'>
+      return <div class={cls.value}>
         {props.title && <span class='mr-6 z-tag-select__title'>{props.title}</span>}
-        {props.options.map((item: any, index: number) => {
-          return <el-tag effect={isActive(item)} class={index === props.options.length - 1 ? 'cursor-pointer' : 'cursor-pointer mr-4'} onClick={() => handleClick(item)}>{item.label}</el-tag>
+        {options.value.map((item: any, index: number) => {
+          return <el-tag effect={isActive(item)} class={index === options.value.length - 1 ? 'cursor-pointer' : 'cursor-pointer mr-4'} onClick={() => handleClickTag(item)}>{item.label}</el-tag>
         })}
+        <span onClick={handleChangeExpand} class="z-tag-select__expand">
+          {isShowMore.value ? '收起' : '展开'}<el-icon class={iconClass.value}><i-arrow-down /></el-icon>
+        </span>
       </div>
     }
   },
