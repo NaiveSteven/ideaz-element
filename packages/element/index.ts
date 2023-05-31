28efce8a8a30654290e67e18d8affd5950cb8c82
NaiveSteven <1004537433@qq.com>
2023-06-01 00:41:16 +0800

    refactor: rename z-description component

--- packages/element/index.ts
+++ packages/element/index.ts
@@ -10,7 +10,7 @@ import TagSelect from './tag-select'
 import ZText from './text'
 import ZWatermark from './watermark'
 import CheckCard from './check-card'
-import ZDescription from './description'
+import ZDescription from './descriptions'
 import { makeInstaller } from './makeInstaller'
 
 const { ZForm, ZFilterForm } = Form
