b50713b4d497bb5926cbab90678c14d3d57f02d6
NaiveSteven <1004537433@qq.com>
2023-06-18 00:36:09 +0800

    feat: change z-form attributes

--- packages/element/form/hooks/useFormItemSlots.tsx
+++ packages/element/form/hooks/useFormItemSlots.tsx
@@ -1,40 +1,49 @@
-import { isFunction } from '@ideaz/utils';
-import FormItemLabel from '../src/FormItemLabel';
+import { isFunction } from '@ideaz/utils'
+import type { Slot } from 'vue'
+import type { FormItemProps } from '../src/props'
+import FormItemLabel from '../src/FormItemLabel'
 
-export const useFormItemSlots = (props: any, slots: any) => {
+export interface Slots {
+  [name: string]: undefined | string | (() => JSX.Element) | Slot
+}
+
+export const useFormItemSlots = (props: FormItemProps, slots: any) => {
   const vSlots = computed<any>(() => {
-    const { col, formConfig } = props;
-    const vSlots: any = {};
-    if (col.formItem?.label || col.frontSlot) {
+    const { col, formConfig } = props
+    const vSlots: Slots = {} as Slots
+    if (col.formItemProps?.label || col.frontSlot || col.label) {
       vSlots.label = () => {
-        return isFunction(slots[col.frontSlot]) ? (
-          slots[col.frontSlot]!()
-        ) : (
+        return isFunction(slots[col.frontSlot!])
+          ? (
+              slots[col.frontSlot!]()
+            )
+          : (
           <FormItemLabel
             {...{
-              ...col.formItem,
+              label: col.label,
+              tooltip: col.tooltip,
+              ...col.formItemProps,
               colon: Object.prototype.hasOwnProperty.call(
-                col.formItem || {},
-                'colon'
+                col.formItemProps || {},
+                'colon',
               )
-                ? col.formItem.colon
+                ? col.formItemProps?.colon
                 : formConfig.colon,
             }}
           />
-        );
-      };
+            )
+      }
     }
 
     if (
-      col.rearSlot &&
-      slots[col.rearSlot] &&
-      isFunction(slots[col.rearSlot])
+      col.rearSlot
+      && isFunction(slots[col.rearSlot])
     ) {
       vSlots.error = () =>
-        isFunction(slots[col.rearSlot]) && slots[col.rearSlot]!();
+        isFunction(slots[col.rearSlot!]) && slots[col.rearSlot!]()
     }
-    return vSlots;
-  });
+    return vSlots
+  })
 
-  return { vSlots };
-};
+  return { vSlots }
+}
