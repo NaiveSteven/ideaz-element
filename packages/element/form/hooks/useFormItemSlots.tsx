5188b88f8f06e471f6986af642141746c553b462
mjqin <mjqin@che300.com>
2023-07-13 11:34:40 +0800

    feat: optimize z-form dts and locale

--- packages/element/form/hooks/useFormItemSlots.tsx
+++ packages/element/form/hooks/useFormItemSlots.tsx
@@ -3,11 +3,11 @@ import type { FormItemProps } from '../src/props'
 import FormItemLabel from '../src/FormItemLabel'
 import type { Slots } from '~/types'
 
-export const useFormItemSlots = (props: FormItemProps, slots: any) => {
+export const useFormItemSlots = (props: FormItemProps, slots: Slots) => {
   const getContent = () => {
     const { col } = props
     if (isSlot(col.label) && isFunction(slots[col.label as string]))
-      return slots[col.label as string]()
+      return (slots[col.label as string] as () => VNode)()
 
     if (isFunction(col.label))
       return col.label()
@@ -43,10 +43,10 @@ export const useFormItemSlots = (props: FormItemProps, slots: any) => {
         vSlots.error = () => (slots[error!] as (() => VNode))()
       }
       else if (isFunction(col.error) && isFunction(slots.error)) {
-        vSlots.error = () => slots.error()
+        vSlots.error = () => (slots.error as () => VNode)()
       }
       else {
-        vSlots.error = () => slots.error()
+        vSlots.error = () => (slots.error as () => VNode)()
       }
     }
     return vSlots
