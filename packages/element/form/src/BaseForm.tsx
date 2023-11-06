560ff5aa653735d73b2271748f88f7f1a7ceee4b
NaiveSteven <1004537433@qq.com>
2023-11-06 23:58:16 +0800

    fix: fix z-form collapse modelValue

--- packages/element/form/src/BaseForm.tsx
+++ packages/element/form/src/BaseForm.tsx
@@ -4,6 +4,7 @@ import { cloneDeep, omit } from 'lodash-unified'
 import { Plus } from '@element-plus/icons-vue'
 import { getContentByRenderAndSlot } from '@ideaz/shared'
 import { isFunction, isString } from '@ideaz/utils'
+import type { CollapseModelValue } from 'element-plus'
 import { ElButton, ElCollapse, ElCollapseItem, ElDivider, ElForm, ElFormItem, ElStep, ElSteps } from 'element-plus'
 import type { ComponentInternalInstance } from 'vue'
 import {
@@ -126,8 +127,8 @@ export default defineComponent({
           modelValue={activeCollapse}
           accordion={accordion}
           class={ns.b('collapse')}
-          onUpdate:activeCollapse={(val: string[]) => { emit('update:activeCollapse', val) }}
-          onChange={(val: string[]) => { emit('collapse-change', val) }}
+          onUpdate:modelValue={(val: CollapseModelValue) => { emit('update:activeCollapse', val) }}
+          onChange={(val: CollapseModelValue) => { emit('collapse-change', val) }}
         >
           {formatFormItems.value.map((column) => {
             if (column.label && column.children && column.children.length) {
