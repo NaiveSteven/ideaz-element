8410572e7112d26770410f6b36adbb8aac5ff352
NaiveSteven <1004537433@qq.com>
2024-01-03 01:15:20 +0800

    refactor: optimize z-input doc

---
+++ docs/demo/input/icon.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import { h, ref, resolveComponent } from 'vue'
+
+const val = ref('')
+const value = ref('')
+
+const setPrefixIcon = () => {
+  return h(resolveComponent('el-icon'), {}, () => h(resolveComponent('i-search')))
+}
+
+const setSuffixIcon = () => {
+  return h(resolveComponent('el-icon'), {}, () => h(resolveComponent('i-calendar')))
+}
+</script>
+
+<template>
+  <z-input v-model="val" clearable placeholder="请输入" prefix-icon="i-search" suffix-icon="i-calendar" class="mb-2" />
+  <z-input v-model="value" clearable placeholder="请输入" :prefix="setPrefixIcon" :suffix="setSuffixIcon" class="mb-2" />
+  <z-input v-model="value" clearable placeholder="请输入">
+    <template #prefix>
+      <el-icon>
+        <i-search />
+      </el-icon>
+    </template>
+    <template #suffix>
+      <el-icon>
+        <i-calendar />
+      </el-icon>
+    </template>
+  </z-input>
+</template>
---
+++ docs/demo/input/normal.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const val = ref('')
+</script>
+
+<template>
+  <z-input v-model="val" clearable placeholder="请输入" />
+</template>
