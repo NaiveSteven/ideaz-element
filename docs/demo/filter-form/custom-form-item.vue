bf80c60eec8e1c8df7530b1f90a60a838ea3fb51
NaiveSteven <1004537433@qq.com>
2024-01-03 00:36:00 +0800

    feat: optimize z-filter-form doc

---
+++ docs/demo/filter-form/custom-form-item.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+import { h, ref } from 'vue'
+
+const formRef = ref()
+const formData = ref({
+  name: '',
+  age: '',
+  height: '身高自定义',
+})
+
+const columns = [
+  {
+    component: 'input',
+    field: 'name',
+    label: '姓名',
+  },
+  {
+    slot: 'ageSlot',
+    label: '年龄',
+  },
+  {
+    render: () => h('span', {}, formData.value.height),
+    label: '身高',
+  },
+]
+</script>
+
+<template>
+  <z-filter-form
+    ref="formRef"
+    v-model="formData"
+    :columns="columns"
+    size="small"
+    label-width="80px"
+  >
+    <template #ageSlot>
+      <el-input v-model="formData.age" placeholder="请输入年龄" clearable />
+    </template>
+  </z-filter-form>
+</template>
