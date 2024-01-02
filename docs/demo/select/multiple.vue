399ce92a26ccfd896a61e156a664e38991787263
NaiveSteven <1004537433@qq.com>
2024-01-03 01:25:48 +0800

    refactor: optimize z-select doc

---
+++ docs/demo/select/multiple.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const options = ref([
+  { label: '选项一', value: 1 },
+  { label: '选项二', value: 2 },
+  { label: '选项三', value: 3 },
+])
+
+const selectVal = ref([1])
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <z-select v-model="selectVal" :options="options" multiple />
+    <z-select
+      v-model="selectVal"
+      class="mt-4"
+      :options="options"
+      collapse-tags
+      collapse-tags-tooltip
+      multiple
+    />
+  </div>
+</template>
---
+++ docs/demo/select/slot.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import { h, ref, resolveComponent } from 'vue'
+
+const options = ref([])
+
+const selectVal = ref()
+
+const setHeader = () => {
+  return h(resolveComponent('el-icon'), {}, () => h(resolveComponent('i-search')))
+}
+
+const setEmpty = () => {
+  return h('div', {}, '自定义内容')
+}
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <z-select v-model="selectVal" :options="options" :prefix="setHeader" :empty="setEmpty" />
+    <z-select v-model="selectVal" :options="options" prefix="header" empty="自定义内容" class="mt-4" />
+    <z-select v-model="selectVal" :options="options" class="mt-4">
+      <template #prefix>
+        <span>头部</span>
+      </template>
+      <template #empty>
+        <span>自定义内容</span>
+      </template>
+    </z-select>
+  </div>
+</template>
