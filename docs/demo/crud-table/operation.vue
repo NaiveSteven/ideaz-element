2734f37a30510d4fe60ff4d4af226abd933c1127
NaiveSteven <1004537433@qq.com>
2024-01-04 01:24:55 +0800

    refactor: optimize crud-table doc

---
+++ docs/demo/crud-table/operation.vue
@@ -0,0 +1,98 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const columns = ref([
+  {
+    label: '姓名',
+    prop: 'name',
+  },
+  {
+    prop: 'sex',
+    label: '性别',
+  },
+  {
+    prop: 'age',
+    label: '年龄',
+  },
+  {
+    prop: 'time',
+    label: '出生日期',
+  },
+  {
+    type: 'button',
+    label: '操作',
+    buttons: [
+      { label: '查看', link: true, type: 'primary', onClick: row => console.log(row, 'row') },
+      { label: '删除', link: true, type: 'danger', onClick: row => console.log(row, 'row') },
+    ],
+  },
+])
+const request = ref({
+  searchApi: getTableData,
+})
+const tableData = ref([])
+const pagination = ref({
+  page: 1,
+  pageSize: 2,
+  total: 0,
+})
+const loading = ref(false)
+
+function getTableData(params) {
+  console.log(params, 'getTableData params')
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const data = [
+        {
+          id: 1,
+          name: 'Steven',
+          sex: 'male',
+          age: 22,
+          time: '2020-01-01',
+        },
+        {
+          id: 2,
+          name: 'Helen',
+          sex: 'male',
+          age: 12,
+          time: '2012-01-01',
+        },
+        {
+          id: 3,
+          name: 'Nancy',
+          sex: 'female',
+          age: 18,
+          time: '2018-01-01',
+        },
+        {
+          id: 4,
+          name: 'Jack',
+          sex: 'male',
+          age: 28,
+          time: '2028-01-01',
+        },
+      ]
+
+      resolve({
+        data: {
+          page: 1,
+          pageSize: 2,
+          total: 4,
+          list: data.slice((pagination.value.page - 1) * pagination.value.pageSize, pagination.value.page * pagination.value.pageSize),
+        },
+      })
+    }, 100)
+  })
+}
+</script>
+
+<template>
+  <z-crud
+    v-model:data="tableData"
+    v-model:pagination="pagination"
+    v-model:loading="loading"
+    :columns="columns"
+    :request="request"
+    :action="false"
+  />
+</template>
