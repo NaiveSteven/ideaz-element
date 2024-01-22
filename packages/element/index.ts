ff8971f8c66989c5798e5a1b7c847add09881026
NaiveSteven <1004537433@qq.com>
2024-01-22 23:55:42 +0800

    feat: add z-form-item export

--- packages/element/index.ts
+++ packages/element/index.ts
@@ -16,14 +16,13 @@ import ZCrud from './crud'
 import Dialog from './dialog'
 import { makeInstaller } from './makeInstaller'
 
-const { ZForm, ZFilterForm } = Form
+const { ZForm, ZFilterForm, ZFormItem } = Form
 const { ZTable, ZTableCustomColumnContainer } = Table
 const { ZCheckCard, ZCheckCardItem } = CheckCard
 const { ZTagSelect, ZTagSelectItem } = TagSelect
 const { ZDialog, ZDialogTip } = Dialog
 
-const components = [ZCheckbox, ZRadio, ZSelect, ZRow, ZCol, ZForm, ZInput,
-  ZTable, ZFilterForm, ZTagSelect, ZText, ZWatermark, ZCheckCard, ZCheckCardItem, ZDescription, ZTagSelectItem, ZTableCustomColumnContainer, ZFullScreen, ZCrud, ZDialog, ZDialogTip]
+const components = [ZCheckbox, ZRadio, ZSelect, ZRow, ZCol, ZForm, ZInput, ZTable, ZFilterForm, ZTagSelect, ZText, ZWatermark, ZCheckCard, ZCheckCardItem, ZDescription, ZTagSelectItem, ZTableCustomColumnContainer, ZFullScreen, ZCrud, ZDialog, ZDialogTip, ZFormItem]
 
 const installer = makeInstaller([...components])
 
@@ -31,6 +30,26 @@ export default installer
 export * from './types'
 
 export {
-  ZRadio, ZSelect, ZCheckbox, ZRow, ZCol, ZForm, ZInput, ZTable, ZFilterForm,
-  ZTagSelect, ZText, ZWatermark, ZCheckCard, ZCheckCardItem, ZDescription, ZTagSelectItem, ZTableCustomColumnContainer, ZFullScreen, ZCrud, ZDialog, ZDialogTip,
+  ZRadio,
+  ZSelect,
+  ZCheckbox,
+  ZRow,
+  ZCol,
+  ZForm,
+  ZInput,
+  ZTable,
+  ZFilterForm,
+  ZTagSelect,
+  ZText,
+  ZWatermark,
+  ZCheckCard,
+  ZCheckCardItem,
+  ZDescription,
+  ZTagSelectItem,
+  ZTableCustomColumnContainer,
+  ZFullScreen,
+  ZCrud,
+  ZDialog,
+  ZDialogTip,
+  ZFormItem,
 }
