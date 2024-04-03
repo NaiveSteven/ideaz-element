8748589335c3a8ef7f1fbe8b2b71407443a55b82
NaiveSteven <1004537433@qq.com>
2024-04-04 02:31:20 +0800

    feat: optimize ZCrud toolBar size

--- packages/element/table/src/ToolBar.tsx
+++ packages/element/table/src/ToolBar.tsx
@@ -52,6 +52,7 @@ export default defineComponent({
     } = useFixedTableCols(props, emit, checkedTableCols)
     const ns = useNamespace('table-tool-bar')
     const { t } = useLocale()
+    const size = useFormSize()
 
     const isIndeterminate = ref(getIsIndeterminate(leftCheckedTableColsUids.value, checkedTableCols.value, rightCheckedTableColsUids.value))
     const checkAll = ref(getIsCheckAll(leftCheckedTableColsUids.value, checkedTableCols.value, rightCheckedTableColsUids.value))
@@ -221,7 +222,7 @@ export default defineComponent({
       return (
         <div class={ns.b('')}>
           <el-tooltip effect="dark" content={t('common.refresh')} placement="top" showAfter={300}>
-            <el-button v-loading={loading} icon={Refresh} text onClick={handleRefresh}></el-button>
+            <el-button v-loading={loading} size={size.value} icon={Refresh} text onClick={handleRefresh}></el-button>
           </el-tooltip>
           <el-tooltip effect="dark" content={t('table.density')} placement="top" showAfter={300}>
             <el-dropdown
@@ -250,7 +251,7 @@ export default defineComponent({
           </el-tooltip>
           <el-tooltip effect="dark" content={isFullScreen.value ? t('common.cancelFullScreen') : t('common.fullScreen')} placement="top" showAfter={300}>
             <z-full-screen el={fullScreenElement || document.getElementsByClassName('z-table__container')[0]} onChange={(val: boolean) => isFullScreen.value = val}>
-              <el-button icon={FullScreen} text />
+              <el-button icon={FullScreen} text size={size.value} />
             </z-full-screen>
           </el-tooltip>
           <el-tooltip effect="dark" content={t('table.columnSetting')} placement="top" showAfter={300}>
@@ -261,7 +262,7 @@ export default defineComponent({
                 trigger="click"
                 v-slots={{
                   reference: () => (
-                    <el-button icon={Operation} text></el-button>
+                    <el-button icon={Operation} text size={size.value}></el-button>
                   ),
                 }}
               >
