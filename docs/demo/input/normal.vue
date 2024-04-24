c2cf9c69ad5aa983c0b523b5c9e662a5fc9592fa
NaiveSteven <1004537433@qq.com>
2024-04-25 01:25:45 +0800

    feat: optimize ZInput component props and code

--- docs/demo/input/normal.vue
+++ docs/demo/input/normal.vue
@@ -2,8 +2,13 @@
 import { ref } from 'vue'
 
 const val = ref('')
+
+function handleInputChange(val: string) {
+  // eslint-disable-next-line no-console
+  console.log(val)
+}
 </script>
 
 <template>
-  <z-input v-model="val" clearable placeholder="请输入" />
+  <z-input v-model="val" clearable placeholder="请输入" @change="handleInputChange" />
 </template>
--- packages/element/input/src/index.tsx
+++ packages/element/input/src/index.tsx
@@ -1,56 +1,33 @@
-import { isVue3 } from 'vue-demi'
-import { reactiveOmit } from '@vueuse/core'
-import { isFunction } from '@ideaz/utils'
+import { ElInput } from 'element-plus'
+import { omit } from 'lodash-unified'
 import { useInputMethods } from '../hooks'
-import { INPUT_SLOTS, inputEmits, inputProps } from './input'
+import { FILTER_INPUT_PROPS, INPUT_SLOTS, inputEmits, zInputProps } from './input'
 
 export default defineComponent({
   name: 'ZInput',
   inheritAttrs: false,
-  props: inputProps,
+  props: zInputProps,
   emits: inputEmits,
-  setup: (props, { emit, slots, listeners = {}, attrs }) => {
-    const { vModelVal, handleInput } = useVModel(props, emit)
+  setup: (props, { emit, slots }) => {
+    const { vModelVal } = useVModel(props, emit)
     const { scopedSlots } = useFormComponentSlots(props, slots, INPUT_SLOTS)
     const { focus, blur, select, clear, resizeTextarea } = useInputMethods()
     const size = useFormSize()
+    const attrs = useAttrs()
     useExpose({ focus, blur, select, clear, resizeTextarea })
 
     return () => {
-      if (isVue3) {
-        const vue3Props = reactiveOmit(props, 'value', 'prefix')
-
-        return (
-          <el-input
-            ref="inputRef"
-            {...vue3Props}
-            {...attrs}
-            size={size.value}
-            modelValue={vModelVal.value}
-            onUpdate:modelValue={(val: string) => (vModelVal.value = val)}
-            onInput={handleInput}
-            v-slots={scopedSlots.value}
-          />
-        )
-      }
-      else {
-        return (
-          <el-input
-            ref="inputRef"
-            {...{ props: { ...props, ...attrs, size: size.value } }}
-            {...{ on: listeners }}
-            value={vModelVal.value}
-            onInput={handleInput}
-          >
-            {INPUT_SLOTS.map((slot) => {
-              if (isFunction(scopedSlots.value[slot]))
-                return <template slot={slot}>{(scopedSlots.value[slot] as () => VNode)()}</template>
-
-              return null
-            })}
-          </el-input>
-        )
-      }
+      return (
+        <ElInput
+          ref="inputRef"
+          {...omit(props, FILTER_INPUT_PROPS)}
+          {...attrs}
+          size={size.value}
+          modelValue={vModelVal.value}
+          onUpdate:modelValue={(val: string) => (vModelVal.value = val)}
+          v-slots={scopedSlots.value}
+        />
+      )
     }
   },
 })
