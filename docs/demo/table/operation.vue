614e2219aeabd0e28b508b09569ce70404509123
NaiveSteven <1004537433@qq.com>
2024-05-20 20:50:39 +0800

    feat: optimize ZTable component doc

--- docs/demo/table/operation.vue
+++ docs/demo/table/operation.vue
@@ -11,32 +11,13 @@ interface RowData {
 }
 
 const loading = ref(false)
-const tableData = ref([
-  {
-    name: 'Steven',
-    sex: 'male',
-    age: 22,
-    time: '2020-01-01',
-  },
-  {
-    name: 'Helen',
-    sex: 'male',
-    age: 12,
-    time: '2012-01-01',
-  },
-  {
-    name: 'Nancy',
-    sex: 'female',
-    age: 18,
-    time: '2018-01-01',
-  },
-  {
-    name: 'Jack',
-    sex: 'male',
-    age: 28,
-    time: '2028-01-01',
-  },
-])
+const tableData = ref<RowData[]>([])
+const pagination = ref({
+  page: 1,
+  pageSize: 2,
+  total: 0,
+  layout: 'total, sizes, prev, pager, next, jumper',
+})
 
 const columns = ref([
   {
@@ -78,12 +59,73 @@ const columns = ref([
     ],
   },
 ])
+
+function mockApi(params: { page: number, pageSize: number }): Promise<{ result: { page: number, pageSize: number, total: number, list: RowData[] } }> {
+  console.log(params, 'params')
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const dataFirstPage = [
+        {
+          name: 'Steven',
+          sex: 'male',
+          age: 22,
+          time: '2020-01-01',
+        },
+        {
+          name: 'Helen',
+          sex: 'male',
+          age: 12,
+          time: '2012-01-01',
+        },
+      ]
+      const dataSecondPage = [
+        {
+          name: 'Nancy',
+          sex: 'female',
+          age: 18,
+          time: '2018-01-01',
+        },
+        {
+          name: 'Jack',
+          sex: 'male',
+          age: 28,
+          time: '2028-01-01',
+        },
+      ]
+      resolve({
+        result: {
+          page: 1,
+          pageSize: 10,
+          total: 4,
+          list: pagination.value.page === 1 ? dataFirstPage : dataSecondPage,
+        },
+      })
+    }, 100)
+  })
+}
+
+async function getTableData() {
+  loading.value = true
+  try {
+    const res = await mockApi({ ...pagination.value })
+    tableData.value = res.result.list
+    pagination.value.total = res.result.total
+  }
+  catch (error) {
+    console.log(error)
+  }
+  loading.value = false
+}
+
+getTableData()
 </script>
 
 <template>
   <z-table
-    :data="tableData"
+    v-model:pagination="pagination"
+    v-model:data="tableData"
+    v-model:columns="columns"
     :loading="loading"
-    :columns="columns"
+    @refresh="getTableData"
   />
 </template>
--- docs/demo/table/title.vue
+++ docs/demo/table/title.vue
@@ -1,33 +1,22 @@
+<!-- eslint-disable no-console -->
 <script lang="ts" setup>
 import { ref } from 'vue'
 
+interface RowData {
+  name: string
+  sex: string
+  age: number
+  time: string
+}
+
 const loading = ref(false)
-const tableData = ref([
-  {
-    name: 'Steven',
-    sex: 'male',
-    age: 22,
-    time: '2020-01-01',
-  },
-  {
-    name: 'Helen',
-    sex: 'male',
-    age: 12,
-    time: '2012-01-01',
-  },
-  {
-    name: 'Nancy',
-    sex: 'female',
-    age: 18,
-    time: '2018-01-01',
-  },
-  {
-    name: 'Jack',
-    sex: 'male',
-    age: 28,
-    time: '2028-01-01',
-  },
-])
+const tableData = ref<RowData[]>([])
+const pagination = ref({
+  page: 1,
+  pageSize: 2,
+  total: 0,
+  layout: 'total, sizes, prev, pager, next, jumper',
+})
 
 const columns = ref([
   {
@@ -47,20 +36,74 @@ const columns = ref([
     label: '出生日期',
   },
 ])
-const pagination = ref({
-  page: 1,
-  pageSize: 10,
-  total: 50,
-  layout: 'prev, pager, next, sizes',
-})
+
+function mockApi(params: { page: number, pageSize: number }): Promise<{ result: { page: number, pageSize: number, total: number, list: RowData[] } }> {
+  console.log(params, 'params')
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const dataFirstPage = [
+        {
+          name: 'Steven',
+          sex: 'male',
+          age: 22,
+          time: '2020-01-01',
+        },
+        {
+          name: 'Helen',
+          sex: 'male',
+          age: 12,
+          time: '2012-01-01',
+        },
+      ]
+      const dataSecondPage = [
+        {
+          name: 'Nancy',
+          sex: 'female',
+          age: 18,
+          time: '2018-01-01',
+        },
+        {
+          name: 'Jack',
+          sex: 'male',
+          age: 28,
+          time: '2028-01-01',
+        },
+      ]
+      resolve({
+        result: {
+          page: 1,
+          pageSize: 10,
+          total: 4,
+          list: pagination.value.page === 1 ? dataFirstPage : dataSecondPage,
+        },
+      })
+    }, 100)
+  })
+}
+
+async function getTableData() {
+  loading.value = true
+  try {
+    const res = await mockApi({ ...pagination.value })
+    tableData.value = res.result.list
+    pagination.value.total = res.result.total
+  }
+  catch (error) {
+    console.log(error)
+  }
+  loading.value = false
+}
+
+getTableData()
 </script>
 
 <template>
   <z-table
     v-model:pagination="pagination"
-    :data="tableData"
+    v-model:data="tableData"
+    v-model:columns="columns"
     :loading="loading"
-    :columns="columns"
     title="表格标题"
+    @refresh="getTableData"
   />
 </template>
