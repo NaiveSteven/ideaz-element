bf90a8c94ad8bc32c0b239e4f4dac64c5832d1fd
NaiveSteven <1004537433@qq.com>
2024-06-09 17:59:46 +0800

    fix: fix ZSelect test issue

--- packages/element/select/__tests__/select.test.ts
+++ packages/element/select/__tests__/select.test.ts
@@ -1,30 +1,31 @@
 import type { VueWrapper } from '@vue/test-utils'
 import { config, mount } from '@vue/test-utils'
 import { ElOption, ElOptionGroup, ElSelect } from 'element-plus'
-import { afterAll, afterEach, describe, expect, test } from 'vitest'
+import { afterAll, afterEach, describe, expect, it } from 'vitest'
 import ZSelect from '../src/index'
 import type { OptionsItem } from '../../types'
 
 config.global.components = { ZSelect, ElSelect, ElOptionGroup, ElOption }
 
-const options: OptionsItem[] = [{ label: 'Jack', value: 'Jack' },
-  { label: 'Rose', value: 'Rose' }, { label: 'Alice', value: 'Alice' }, { label: 'Steven', value: 'Steven' }, { label: 'Bob', value: 'Bob', disabled: true }]
+const options: OptionsItem[] = [{ label: 'Jack', value: 'Jack' }, { label: 'Rose', value: 'Rose' }, { label: 'Alice', value: 'Alice' }, { label: 'Steven', value: 'Steven' }, { label: 'Bob', value: 'Bob', disabled: true }]
 
-const getInputValue = (wrapper: VueWrapper<ComponentPublicInstance>) =>
-  wrapper.find<HTMLInputElement>('.el-input__inner').element.value
-const getOptions = () =>
-  Array.from(
+function getInputValue(wrapper: VueWrapper<ComponentPublicInstance>, index: number) {
+  return (wrapper.findAll('.el-select__placeholder').at(index) as any).text()
+}
+function getOptions() {
+  return Array.from(
     document.querySelectorAll<HTMLElement>(
       'body > div:last-child .el-select-dropdown__item',
     ),
   )
+}
 
 describe('select', () => {
   afterEach(() => {
     document.body.innerHTML = ''
   })
 
-  test('options and value change', async () => {
+  it('options and value change', async () => {
     const wrapper = mount({
       template: '<z-select v-model="value" :options="options" />',
       setup() {
@@ -33,29 +34,29 @@ describe('select', () => {
       },
     })
 
-    await wrapper.find('.select-trigger').trigger('click')
+    await wrapper.find('.el-select__wrapper').trigger('click')
     const data = getOptions()
     expect(wrapper.vm.value).toBe('')
-    expect(getInputValue(wrapper)).toBe('')
+    expect(getInputValue(wrapper, 0)).toBe('Select')
 
     data[2].click()
     await nextTick()
     expect(wrapper.vm.value).toBe('Alice')
-    expect(getInputValue(wrapper)).toBe('Alice')
+    expect(getInputValue(wrapper, 0)).toBe('Alice')
 
     data[3].click()
     await nextTick()
     expect(wrapper.vm.value).toBe('Steven')
-    expect(getInputValue(wrapper)).toBe('Steven')
+    expect(getInputValue(wrapper, 0)).toBe('Steven')
 
     // disabled
     data[4].click()
     await nextTick()
     expect(wrapper.vm.value).toBe('Steven')
-    expect(getInputValue(wrapper)).toBe('Steven')
+    expect(getInputValue(wrapper, 0)).toBe('Steven')
   })
 
-  test('options change', async () => {
+  it('options change', async () => {
     const wrapper = mount({
       template: '<z-select v-model="value" :options="opts" />',
       setup() {
@@ -65,10 +66,10 @@ describe('select', () => {
       },
     })
 
-    await wrapper.find('.select-trigger').trigger('click')
+    await wrapper.find('.el-select__wrapper').trigger('click')
     await (wrapper.vm.opts as OptionsItem[]).push({ value: 'Helen', label: 'Helen' })
     expect(wrapper.vm.value).toBe('Steven')
-    expect(getInputValue(wrapper)).toBe('Steven')
+    expect(getInputValue(wrapper, 0)).toBe('Steven')
     const data = getOptions()
 
     data[5].click()
@@ -76,7 +77,7 @@ describe('select', () => {
     expect(wrapper.vm.value).toBe('Helen')
   })
 
-  test('alias', async () => {
+  it('alias', async () => {
     const wrapper = mount({
       template: '<z-select v-model="value" :options="opts" :alias="alias"/>',
       setup() {
@@ -90,7 +91,7 @@ describe('select', () => {
       },
     })
 
-    await wrapper.find('.select-trigger').trigger('click')
+    await wrapper.find('.el-select__wrapper').trigger('click')
     const data = getOptions()
     const list = data.map(item => item.querySelector('span')?.innerHTML)
     ;['Jack', 'Rose', 'Alice', 'Steven', 'Bob'].forEach((item) => {
@@ -100,10 +101,10 @@ describe('select', () => {
     data[3].click()
     await nextTick()
     expect(wrapper.vm.value).toBe('Steven')
-    expect(getInputValue(wrapper)).toBe('Steven')
+    expect(getInputValue(wrapper, 0)).toBe('Steven')
   })
 
-  test('group type', async () => {
+  it('group type', async () => {
     const wrapper = mount({
       template: '<z-select v-model="value" :options="opts" />',
       setup() {
@@ -119,9 +120,9 @@ describe('select', () => {
       },
     })
 
-    await wrapper.find('.select-trigger').trigger('click')
+    await wrapper.find('.el-select__wrapper').trigger('click')
     expect(wrapper.vm.value).toBe('')
-    expect(getInputValue(wrapper)).toBe('')
+    expect(getInputValue(wrapper, 0)).toBe('Select')
     const data = getOptions()
 
     expect(document.querySelector('body > div:last-child .el-select-group__title')?.innerHTML).toBe('names')
@@ -131,7 +132,7 @@ describe('select', () => {
     })
   })
 
-  test('select all', async () => {
+  it('select all', async () => {
     const wrapper = mount({
       template: '<z-select v-model="value" :options="options" multiple all />',
       setup() {
@@ -140,9 +141,9 @@ describe('select', () => {
       },
     })
 
-    await wrapper.find('.select-trigger').trigger('click')
+    await wrapper.find('.el-select__wrapper').trigger('click')
     expect(wrapper.vm.value).toStrictEqual([])
-    expect(getInputValue(wrapper)).toBe('')
+    expect(getInputValue(wrapper, 0)).toBe('Select')
     const data = getOptions()
 
     const list = data.map(item => item.querySelector('span')?.innerHTML)
@@ -166,7 +167,7 @@ describe('select', () => {
     expect(wrapper.vm.value).toStrictEqual([])
   })
 
-  test('render and slot', async () => {
+  it('render and slot', async () => {
     const wrapper = mount({
       template: '<z-select v-model="value" :options="opts"><template #optionSlot="{option}"><span>自定义Slot{{option.value}}</span></template></z-select>',
       setup() {
@@ -175,8 +176,7 @@ describe('select', () => {
           {
             label: '1',
             value: '1',
-            // eslint-disable-next-line no-empty-pattern
-            render: ({}, { option }: { option: OptionsItem }) => h('span', {}, `自定义Render${option.value}`),
+            render: ({ option }: { option: OptionsItem }) => h('span', {}, `自定义Render${option.value}`),
           },
           {
             label: '2',
@@ -188,9 +188,9 @@ describe('select', () => {
       },
     })
 
-    await wrapper.find('.select-trigger').trigger('click')
+    await wrapper.find('.el-select__wrapper').trigger('click')
     expect(wrapper.vm.value).toBe('')
-    expect(getInputValue(wrapper)).toBe('')
+    // expect(getInputValue(wrapper, 0)).toBe('')
     const data = getOptions()
 
     const list = data.map(item => item.querySelector('span')?.innerHTML)
@@ -199,7 +199,7 @@ describe('select', () => {
     })
   })
 
-  test('prefix', async () => {
+  it('prefix', async () => {
     const wrapper = mount({
       template: '<z-select v-model="value" class="set-prefix" :prefix="setPrefix"/><z-select v-model="value" class="string-prefix" prefix="asdf"/>',
       setup() {
@@ -209,11 +209,11 @@ describe('select', () => {
       },
     })
 
-    expect(wrapper.find('.set-prefix .el-input__prefix-inner').text()).toBe('prefix')
-    expect(wrapper.find('.string-prefix .el-input__prefix-inner').text()).toBe('asdf')
+    expect(wrapper.find('.set-prefix .el-select__prefix').text()).toBe('prefix')
+    expect(wrapper.find('.string-prefix .el-select__prefix').text()).toBe('asdf')
   })
 
-  test('options change', async () => {
+  it('options change1', async () => {
     mount({
       template: '<z-select v-model="value" class="set-empty" :empty="setEmpty"/><z-select v-model="value" class="string-empty" empty="asdf"/>',
       setup() {
