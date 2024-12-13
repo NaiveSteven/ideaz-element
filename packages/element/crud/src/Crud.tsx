35c500cde024d45587a2dfca2be395fd2d3f3c5e
NaiveSteven <1004537433@qq.com>
2024-12-13 23:33:11 +0800

    feat: error handling

--- packages/element/crud/src/Crud.tsx
+++ packages/element/crud/src/Crud.tsx
@@ -208,7 +208,7 @@ export default defineComponent({
     }
 
     const renderSearchForm = () => {
-      return searchFormColumns.value.length > 0 && renderDecorator({
+      return searchFormColumns.value?.length > 0 && renderDecorator({
         ...props.formDecorator,
         style: {
           marginBottom: '16px',
